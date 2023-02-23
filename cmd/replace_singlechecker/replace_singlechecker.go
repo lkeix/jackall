d2d92399a42c8f9bc336633929d47984fda69829
lkeix <ai.kanaria.ai@gmail.com>
2023-02-24 00:42:33 +0900

    add: walk ast and find os Exit process

--- cmd/replace_singlechecker/replace_singlechecker.go
+++ cmd/replace_singlechecker/replace_singlechecker.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"go/ast"
 	"go/build"
 	"go/parser"
 	"go/token"
@@ -13,8 +14,7 @@ import (
 )
 
 func main() {
-	var buf bytes.Buffer
-	replaceSingleChecker(&buf)
+	overlay("")
 }
 
 func overlay(dir string) (string, error) {
@@ -28,27 +28,60 @@ func overlay(dir string) (string, error) {
 }
 
 func replaceSingleChecker(w io.Writer) (string, error) {
-	pkgDir := filepath.Join(build.Default.GOPATH, "pkg/mod/golang.org/x/tools@v0.6.0/go/analysis")
-	pkg, err := build.Default.Import("singlechecker", pkgDir, 0)
-	fmt.Println(pkg)
-	fmt.Println(err)
+	pkgDir := filepath.Join(build.Default.GOPATH, "pkg/mod/golang.org/x/tools@v0.6.0/go/analysis/singlechecker")
+
+	fset := token.NewFileSet()
+	pkgs, err := parser.ParseDir(fset, pkgDir, nil, parser.ParseComments)
 	if err != nil {
 		return "", err
 	}
 
-	fset := token.NewFileSet()
-	pkgs, err := parser.ParseDir(fset, pkg.Dir, nil, parser.ParseComments)
-	if err != nil {
-		return "", err
-	}
+	fmt.Println(pkgs)
 
 	if pkgs["singlechecker"] == nil {
 		return "", errors.New("not found singlechecker package")
 	}
 
 	for _, file := range pkgs["singlechecker"].Files {
-		fmt.Println(file)
+		for _, decl := range file.Decls {
+			decl, ok := decl.(*ast.FuncDecl)
+			if !ok {
+				continue
+			}
+
+			searchForExit(decl.Body)
+			fmt.Println(decl.Name)
+		}
 	}
 	// pkg, err := build.Default.Import("time", srcDir, 0)
 	return "", nil
+}
+
+func searchForExit(body *ast.BlockStmt) {
+	for _, stmt := range body.List {
+		expr, ok := stmt.(*ast.ExprStmt)
+		if !ok {
+			continue
+		}
+
+		rhs, ok := expr.X.(*ast.CallExpr)
+		if !ok {
+			continue
+		}
+
+		sel, ok := rhs.Fun.(*ast.SelectorExpr)
+		if !ok {
+			continue
+		}
+
+		ident, ok := sel.X.(*ast.Ident)
+		if !ok {
+			continue
+		}
+
+		if ident.Name == "os" && sel.Sel.Name == "Exit" {
+			// TODO: exprをコメントアウトする
+			ident.Name = "// os"
+		}
+	}
 }
