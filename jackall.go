5e7e28989a0f1cdb9ff720a69376eceeb554ab8a
lkeix <ai.kanaria.ai@gmail.com>
2023-03-01 22:54:50 +0900

    feat: don't include go get module std package

--- jackall.go
+++ jackall.go
@@ -2,8 +2,11 @@ package jackall
 
 import (
 	"fmt"
+	"go/build"
+	"os/exec"
 	"path/filepath"
 	"regexp"
+	"runtime"
 	"strings"
 
 	"golang.org/x/tools/go/analysis"
@@ -12,8 +15,9 @@ import (
 )
 
 type dependenceVec struct {
-	from string
-	to   string
+	from  string
+	to    string
+	toRaw string
 }
 
 type dependencePair struct {
@@ -22,6 +26,33 @@ type dependencePair struct {
 }
 
 type dependenceVecs []*dependenceVec
+
+func (d dependenceVecs) contain(pkg string) bool {
+	for _, v := range d {
+		if v.toRaw == pkg {
+			return true
+		}
+	}
+	return false
+}
+
+func (d dependenceVecs) filter(pkgs []string) dependenceVecs {
+	res := make(dependenceVecs, 0)
+
+	for _, vec := range d {
+		isThirdParty := false
+		for _, pkg := range pkgs {
+			if vec.toRaw == pkg {
+				isThirdParty = true
+			}
+		}
+		if !isThirdParty {
+			res = append(res, vec)
+		}
+	}
+
+	return res
+}
 
 func (d dependenceVecs) extractVecEachPackage() map[string]*dependencePair {
 	mp := make(map[string]*dependencePair)
@@ -51,6 +82,7 @@ func (d dependenceVecs) extractVecEachPackage() map[string]*dependencePair {
 
 	return mp
 }
+
 func Run() {
 	vec := make(dependenceVecs, 0)
 
@@ -65,6 +97,16 @@ func Run() {
 
 	singlechecker.Main(analyzer)
 
+	vec, err := removeStdPackages(vec)
+	if err != nil {
+		panic(err)
+	}
+
+	vec, err = removeThirdPartyPackages(vec)
+	if err != nil {
+		panic(err)
+	}
+
 	res := vec.extractVecEachPackage()
 	for name, r := range res {
 		fmt.Printf("degree of dependency in %s package: %.4f\n", name, float64(r.out)/float64(r.in+r.out))
@@ -72,6 +114,37 @@ func Run() {
 
 	fmt.Printf("the closer degree of dependency is 1, the less stable(unstable) package is\n")
 	fmt.Printf("the closer degree of dependency is 0, the more stable package is\n")
+}
+
+func removeStdPackages(vecs dependenceVecs) (dependenceVecs, error) {
+	res := make(dependenceVecs, 0)
+	srcDir := filepath.Join(runtime.GOROOT(), "src")
+
+	for _, vec := range vecs {
+		if _, err := build.Default.Import(vec.to, srcDir, 0); err != nil {
+			res = append(res, vec)
+		}
+	}
+	return res, nil
+}
+
+func removeThirdPartyPackages(vecs dependenceVecs) (dependenceVecs, error) {
+	cmd := exec.Command("go", "list", "-m", "all")
+	out, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	rows := strings.Split(string(out), "\n")
+	pkgs := make([]string, 0)
+	for _, row := range rows {
+		col := strings.Split(row, " ")
+		if len(col) == 2 {
+			pkgs = append(pkgs, col[0])
+		}
+	}
+
+	return vecs.filter(pkgs), nil
 }
 
 // wrapRun bind import dependency for arguments struct
@@ -83,8 +156,9 @@ func wrapRun(vec *dependenceVecs) func(pass *analysis.Pass) (interface{}, error)
 			for _, imprt := range f.Imports {
 				name := extractImportPackageName(imprt.Path.Value)
 				*vec = append(*vec, &dependenceVec{
-					from: f.Name.Name,
-					to:   name,
+					from:  f.Name.Name,
+					to:    name,
+					toRaw: strings.ReplaceAll(imprt.Path.Value, "\"", ""),
 				})
 			}
 		}
